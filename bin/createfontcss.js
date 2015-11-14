1c0a5152bf39adb49543cdd86d80b9469714ce25
Mikael Lindqvist <li.mikael@gmail.com>
2015-11-14 09:10:16 +0300

    command line program

---
+++ bin/createfontcss.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var FontCssCreator = require("../src/FontCssCreator");
+var minimist = require("minimist");
+
+function usage() {
+	console.log("createfontcss - Create css file suitable to load web fonts.");
+	console.log("");
+	console.log("Usage:");
+	console.log("  createfontcss [options] <font files [...]>");
+	console.log("");
+	console.log("Options:");
+	console.log("  --css=<filename.css>    - File to wirte css to.");
+	console.log("                            Default is standard out.");
+	console.log("");
+	process.exit(1);
+}
+
+var args = minimist(process.argv.slice(2));
+
+if (!args._.length)
+	usage();
+
+knownAgrs = ["css"];
+
+for (arg in args) {
+	if (arg != "_" && knownAgrs.indexOf(arg) < 0) {
+		console.log("Unknown argument: " + arg);
+		process.exit(1);
+	}
+}
+
+cssCreator = new FontCssCreator();
+
+cssCreator.parseFontFiles(args._).then(
+	function() {
+		if (args.css) {
+			cssCreator.saveCss(args.css);
+			console.log("Wrote " + args.css);
+		} else
+			console.log(cssCreator.getCss());
+	},
+	function(err) {
+		console.log(err);
+		process.exit(1);
+	}
+)
